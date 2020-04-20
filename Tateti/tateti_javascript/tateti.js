2654197e2b6197645bc225b4a125b3a8dd005e09
ChechiB <chechi.fx2@gmail.com>
2020-04-20 01:19:15 -0300

    Add: eventListener

---
+++ Tateti/tateti_javascript/tateti.js
@@ -0,0 +1,18 @@
+/* 
+    1. pedir los nombres
+    2. iniciar juego 
+        - validar que los campos nombres tengan valor
+        - mostrar la tabla que esta oculta
+
+        - random determinar quien es circulo y quien x
+        - random determinar quien empieza
+        - Mostrar en algun lado a quien le toca y que es
+*/
+
+function foo() {
+    return "bar"
+}
+
+function updateCell(){
+    console.log("hello")
+}
